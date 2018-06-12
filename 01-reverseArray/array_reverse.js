3e94190d9e5526eb983a1e8c2ef582bf5ee959fa
Tracy Williams <selpilot@gmail.com>
2018-06-12 06:10:41 -0700

    added alternate versions

--- 01-reverseArray/array_reverse.js
+++ 01-reverseArray/array_reverse.js
@@ -1,22 +1,67 @@
 'use strict';
 
-function arrayReverse(arr) {
+const useVersion = 1;
 
-  console.log(typeof arr);
-  if (typeof arr !== 'object' || !arr.length) return undefined;
+const versions = [
+  function arrayReverseRev0(arr) {
 
-  let i1 = 0, i2 = arr.length - 1, temp;
+    if (typeof arr !== 'object' || !arr.length) return undefined;
 
-  while (i1 < i2){
-    temp = arr[i2];
-    arr[i2] = arr[i1];
-    arr[i1] = temp;
-    i1++;
-    i2--;
+    let i1 = 0, i2 = arr.length - 1, temp;
+
+    while (i1 < i2){
+      temp = arr[i2];
+      arr[i2] = arr[i1];
+      arr[i1] = temp;
+      i1++;
+      i2--;
+    }
+
+    return arr;
+  },
+
+  function arrayReverseRev1(arr) {
+
+    if (typeof arr !== 'object' || !arr.length) return undefined;
+
+    const swap = (a, b, arr) => [arr[b], arr[a]] = [arr[a], arr[b]];
+
+    for (let i1 = 0, i2 = arr.length-1; i1 < i2; i1++, i2--) {
+      swap(i1, i2, arr);
+    }
+
+    return arr;
+  },
+
+  function arrayReverseRev2(arr) {
+
+    return (typeof arr !== 'object' || !arr.length) ? undefined : ((arr) => {
+
+      const swap = (a, b, arr) => [arr[b], arr[a]] = [arr[a], arr[b]];
+
+      for (let i1 = 0, i2 = arr.length-1; i1 < i2; i1++, i2--) {
+        swap(i1, i2, arr);
+      }
+
+      return arr;
+    })(arr);
+  },
+
+  function arrayReverseRev3(arr) {
+
+    const swap = (a, b, arr) => [arr[b], arr[a]] = [arr[a], arr[b]];
+
+    return (typeof arr !== 'object' || !arr.length) ? undefined : ((arr) => {
+      let newArr = arr.slice(0);
+      arr.map((e, i, arr) => {
+        return (i < arr.length-1-i) ? swap(i, arr.length-1-i, newArr) : e;
+      });
+      return newArr;
+    })(arr);
   }
+];
 
-  return arr;
-}
+const arrayReverse = versions[useVersion];
 
 console.log('[1, 2, 3, 4]', arrayReverse([1, 2, 3, 4])); //good
 console.log('[1, 2, 3]', arrayReverse([1, 2, 3])); //good
